8460d8e98bce7e49c72e3f086060cf189a94704c
sebgutierrez <84500161+sebgutierrez@users.noreply.github.com>
2023-11-06 17:40:01 -0600

    Finished home page

--- script.js
+++ script.js
@@ -43,6 +43,80 @@ const server = http.createServer((req, res) => {
     }
 });
 
-server.listen(3000, () => {
-    console.log('Server is running on port 3000');
+// return json of package tracking history
+function getTrackingData(tracking_id) {
+    // Creating a connection pool
+    sql.connect(config, (err) => {
+        if (!err) {
+            // Create a request instance
+            const request = new sql.Request();
+
+            // Query to the database
+            /*
+            request.query(`select * from addresses`, (err, recordset) => {
+                if (!err) {
+                    console.log(err);
+                    // Print the recordset
+                    recordset.recordset.forEach((row) => {
+                        console.log(row); // This will print each row in the recordset
+                    });
+
+                    sql.close(); // Close the connection pool
+                } 
+                else{
+                    console.log(err);
+                }
+
+            });
+            */
+        }
+        else{
+            console.log(err);
+        }
+    });
+}
+
+server.on('request', (request, response) => {
+	request.on('error', (err) => {
+		console.error('error', (err) => {
+            console.log(err)
+        });
+	});
+	response.on('error', (err) => {
+		console.error('error', (err) => {
+            console.log(err)
+        });
+	});
+	const { method, url } = request
+	if(method === 'POST' && url === '/track'){
+        let body = [];
+		request.on('data', (chunk) => {
+            console.log(chunk)
+			body.push(chunk)
+		});
+        request.on('end', () => {
+            var tracking_id = Buffer.concat(body).toString();
+            console.log(tracking_id)
+        })
+        response.on('end', () => {
+            let tracking_data = getTrackingData(tracking_id)
+            response.writeHead(200, {'Content-Type': 'application/json'})
+            response.end({text: " query successful"})
+        })
+	}
+	else{
+        response.on('end', () => {
+            response.writeHead(404, { 'Content-Type': 'text/plain' });
+            response.end('Track Page Not Found');
+        })
+	}
 });
+
+port = 5500
+hostname = '127.0.0.1'
+
+server.listen(5500, hostname, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+module.exports.server = server
